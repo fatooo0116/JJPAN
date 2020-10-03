fe74b0fc89b37dc461f49becf6509684f915d733
fatooo0116@gmail.com <cbsi940424>
2020-10-03 11:43:38 +0800

    udpate

--- news.php
+++ news.php
@@ -19,33 +19,37 @@
             </div>
 
             
-            <div id="main_text">
+            <div id="main_text" class="hidemore" >
                 <h1>Xiamen Chang Gung Memorial Hospital” wins the Competition</h1>
                 <div class="content">
                     <p>JP is proud to announce its design for the Xiamen Chang Gung Memorial Hospital Competition, has been declared the winner of the competition.</p>
                     <p>Located in Haicang district, Maluan Bay, the Xiamen Chang Gung Memorial Hospital has a site area of 325,800㎡ with a total floor area at 37,600㎡. The hospital consists of one basement level and 23 above grade.</p>
                     <p>Maluan Bay Park is situated to the north of the hospital, while waterways run through the east and west of the site. The hospital also enjoys open views of the water and the Caijianwei Mountain scenery, the number 2 subway line provides transportation accessibility. The architectural design takes inspiration from the lighthouses of Maluan Bay to create a healthcare facility that takes full advantage of the ocean views. Close attention is paid to the circulation routes as well as designing them to be universal accessible facilities. Program elements incorporated in the tower include both living quarters and healthcare elements. Public service areas, restaurants, commercial street fronts, childcare centers, and exhibition zones are placed in the first level.  Multi-function halls, rehabilitation, and fitness center are located on the 2nd level. The podium section consists of nursing and daycare units to provide professional services to the semi-disabled residents.</p>
                 </div>
-                <div class="more_detail"> 
+
+                <a href="#" class="toggle_class">More Details</a>
+            </div>
+
+
+                <div class="more_spec"> 
                     <ul>
                         <li>Location: Xiamen City</li>
                         <li>Location: Xiamen City</li>
                         <li>Location: Xiamen City</li>
                     </ul>
                 </div>
-            </div>
 
 
             <div class="rela_postx">
-                <a href="#" class="prev">PREV</a>
-                <a href="#" class="back"></a>
-                <a href="#" class="next">NEXT</a>
+                <a href="#" class="prev"><i class="fa fa-angle-left" aria-hidden="true"></i> PREV</a>
+                <a href="#" class="back"><i class="fa fa-th-large" aria-hidden="true"></i></a>
+                <a href="#" class="next">NEXT <i class="fa fa-angle-right" aria-hidden="true"></i></a>
             </div>
 
 
 
             <!-- #####   home_news  #####  -->
-            <div id="home_news" >
+            <div id="home_news" class="grey_bk">
               
                 <div class="one_post_box_type2">
                     <a href="#"><img src="assets/dist/img/news_200824_4.jpg" /></a>
--- project_cat3.php
+++ project_cat3.php
@@ -1,120 +1,44 @@
 <?php include "tpl/header.php"; ?>
 <?php include "tpl/menu.php"; ?>
         <div id="main">
+
            <!-- #####   home_news  #####  --> 
-           <div class="top_slider">
-                <div><img src="assets/dist/img/slider1.jpg" /></div>
-                <div><img src="assets/dist/img/slider2.jpg" /></div>
-                <div><img src="assets/dist/img/slider1.jpg" /></div>
-            </div>
+           <h1 class="main_title top">Planning</h1>
 
-
-            <div class="icon_menu">
-                <a href="#" class="link">                    
-                    <div class="icon">
-                        <img src="assets/dist/img/menu_icon1.png">
-                    </div>
-                    <h3>News</h3>
-                </a>
-
-                <a href="#" class="link">                    
-                    <div class="icon">
-                        <img src="assets/dist/img/menu_icon2.png">
-                    </div>
-                    <h3>Projects</h3>
-                </a>      
-                
-                <a href="#" class="link">                    
-                    <div class="icon">
-                        <img src="assets/dist/img/menu_icon3.png">
-                    </div>
-                    <h3>Publications</h3>
-                </a>  
-                
-                <a href="#" class="link">                    
-                    <div class="icon">
-                        <img src="assets/dist/img/menu_icon4.png">
-                    </div>
-                    <h3>Leadership</h3>
-                </a>                  
-            </div>
+            <div class="menu_tab">
+                <div class="inner">
+                    <a href="project_cat1.php" class="tab1"></a>
+                    <a href="project_cat2.php" class="tab2"></a>
+                    <a href="project_cat3.php" class="tab3"></a>
+                </div>
+                <h4>Select display style</h4>
+            </div>    
 
 
 
 
 
             <!-- #####   home_news  #####  -->
-            <div id="home_news" >
-                <div class="one_post_box">
-                    <img src="assets/dist/img/news_200824_4.jpg" />
-                    <div class="desc">
-                        <h3>Experiencing 20 years of Grace” Suang-Lien Elderly Center</h3>
-                        <div class="meta">
-                            <div class="cat news">News</div>
-                            <div class="date">July 22, 2020</div>
-                        </div>
-                    </div>                    
+            <div id="project_archives" >
+                <div class="single_project_3">
+                    <a href="#" >
+                        <div class="pic" style="background-image:url(assets/dist/img/news_200824_4.jpg)" ><img src="assets/dist/img/news_200824_4.jpg" /></div>
+                        <div class="desc">
+                            <h3>Experiencing 20 years of Grace” Suang-Lien Elderly Center</h3>
+                        </div>                    
+                    </a>
                 </div>
 
-                <div class="one_post_box">
-                    <img src="assets/dist/img/news_200824_4.jpg" />
-                    <div class="desc">
-                        <h3>Experiencing 20 years of Grace” Suang-Lien Elderly Center</h3>
-                        <div class="meta">
-                            <div class="cat news">News</div>
-                            <div class="date">July 22, 2020</div>
-                        </div>
-                    </div>                    
-                </div>
-                
-                <div class="one_post_box">
-                    <img src="assets/dist/img/news_200824_4.jpg" />
-                    <div class="desc">
-                        <h3>Experiencing 20 years of Grace” Suang-Lien Elderly Center</h3>
-                        <div class="meta">
-                            <div class="cat news">News</div>
-                            <div class="date">July 22, 2020</div>
-                        </div>
-                    </div>                    
-                </div>
-                
-                <div class="one_post_box">
-                    <img src="assets/dist/img/056000051.jpg" />
-                    <div class="desc">
-                        <h3>Jiaxing Complex with Shopping Mall, Office and Residential</h3>
-                        <div class="meta">
-                            <div class="cat project">Projects</div>
-                            <div class="date">July 22, 2020</div>
-                        </div>
-                    </div>                    
-                </div> 
-                
-                <div class="one_post_box">
-                    <img src="assets/dist/img/056000051.jpg" />
-                    <div class="desc">
-                        <h3>Jiaxing Complex with Shopping Mall, Office and Residential</h3>
-                        <div class="meta">
-                            <div class="cat project">Projects</div>
-                            <div class="date">July 22, 2020</div>
-                        </div>
-                    </div>                    
-                </div>  
-                
-                <div class="one_post_box">
-                    <img src="assets/dist/img/056000051.jpg" />
-                    <div class="desc">
-                        <h3>Jiaxing Complex with Shopping Mall, Office and Residential</h3>
-                        <div class="meta">
-                            <div class="cat project">Projects</div>
-                            <div class="date">July 22, 2020</div>
-                        </div>
-                    </div>                    
-                </div>  
+                <div class="single_project_3">
+                    <a href="#" >
+                        <div class="pic" style="background-image:url(assets/dist/img/news_200824_4.jpg)" ><img src="assets/dist/img/news_200824_4.jpg" /></div>
+                        <div class="desc">
+                            <h3>Experiencing 20 years of Grace” Suang-Lien Elderly Center</h3>
+                        </div>                    
+                    </a>
+                </div>               
 
-            </div>
-
-            <div style="width:100%;min-height:800px"></div>
-
+            </div>           
         </div>
 
 
